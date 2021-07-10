685de8e2394f9e2b2be5800b9b1b15138e97ed74
Yuvraj IN1076515 <gawadey@vmware.com>
2021-07-10 10:12:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Counter from './components/Counter';
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+  return (
+    <Fragment>
+      <Header />
+      { !isAuth ? <Auth /> : <UserProfile /> }
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterSlice from './counter';
+import authSlice from './auth';
+
+const store = configureStore({
+    reducer: {
+      counter: counterSlice.reducer,
+      auth: authSlice.reducer
+    }
+});
+
+export default store;
